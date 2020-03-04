e8b06a1e41bb09c3310ec8ed5487524b11200627
dhruvgar <dhruvgar@umich.edu>
2020-03-03 20:37:18 -0500

    added backspace functionality

--- display.js
+++ display.js
@@ -1,29 +1,41 @@
 let log = console.log;
 var counter = 0;
+var correctTextCounter = 0;
 
 document.addEventListener("DOMContentLoaded", init);
 
 function init() {
   let txt = document.getElementById('in');
   txt.addEventListener('keypress', type);
+  txt.addEventListener('keydown', backspace);
   /*
     used to be keydown. making it keypress fixes shift and case problem.
     see Google Doc for explanation
   */
+}
 
+function backspace(event)
+{
+  var evtKcode = event.keyCode;
+  if (evtKcode == 8)
+  {
+    log("FOUND A BACKSPACE");
+      correctTextCounter--;
+  }
 }
 
 function type(event) {
   let target = event.currentTarget;
   let tag = target.tagName;
   let char = event.which;
-  log("CHAR: " + char)  //subtract ascii value to get to lowercase
+  log("CHAR: " + char);
   let s = String.fromCharCode(char);
   //log(s);
   // add if statements to check if it's a backspace (if it is delete a char)
   document.getElementById('main_text').innerHTML += s;
   compare(counter);
   counter++;
+  correctTextCounter++;
 }
 
 function compare(counter)
@@ -31,9 +43,9 @@ function compare(counter)
   var correct_text = document.getElementById('correct_text').innerHTML;
   var input_text = document.getElementById('main_text').innerHTML;
   log ("COUNTER: " + counter);
-  log ("CORRECT TEXT: " + correct_text[counter]);
+  log ("CORRECT TEXT: " + correct_text[correctTextCounter]);
   log ("INPUT TEXT: " + input_text[counter]);
-  if (correct_text[counter] == input_text[counter])
+  if (correct_text[correctTextCounter] == input_text[counter])
   {
     log("T");
   }
