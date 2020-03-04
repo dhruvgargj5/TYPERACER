b445c229c2acaed9e3fd220b466d746676abadf9
dhruvgar <dhruvgar@umich.edu>
2020-03-04 01:48:54 -0500

    incorrect highlight needs fixing

--- display.js
+++ display.js
@@ -1,6 +1,7 @@
 let log = console.log;
 var counter = 0;
 var correctTextCounter = 0;
+var inputTextCounter = 0;
 
 document.addEventListener("DOMContentLoaded", init);
 
@@ -20,7 +21,7 @@ function backspace(event)
   if (evtKcode == 8)
   {
     log("FOUND A BACKSPACE");
-    //counter--;
+    inputTextCounter--;
   }
 }
 function type(event) {
@@ -35,41 +36,55 @@ function type(event) {
   document.getElementById('input_text').innerHTML += s;
   compare(counter);
   counter++;
+  inputTextCounter++;
   correctTextCounter++;
 }
 
 function compare(counter)
 {
-  var correct_text = document.getElementById('correct_text').innerHTML;
   var input_text = document.getElementById('input_text').innerHTML;
+  var correct_text = "OOH WHOA OOH whoa, ooh whoa You know you love me, I know you care Just shout whenever and I'll be there You are my love, you are my heart And we will never, ever, ever be apart";
+//log ("ALL OF CORRECT TEXT: " + correct_text);
   log ("COUNTER: " + counter);
   log ("CORRECT TEXT: " + correct_text[correctTextCounter]);
   log ("INPUT TEXT: " + input_text[counter]);
+  log ("INPUT TEXT COUNTER: " + inputTextCounter);
+  log ("CORRECT TEXT COUNTER: " + correctTextCounter);
   if (correct_text[correctTextCounter] == input_text[counter])
   {
     log("T");
+    //clears input space after every word
     if (input_text[counter] == " ")
     {
       document.getElementById('in').value = ''
     }
-    var cText = document.getElementById('correct_text');
-    cText.style.color = 'blue';
+    // var cText = document.getElementById('correct_text');
+    // cText.style.color = 'blue';
+    correctHighlight();
   }
   else {
       log("F");
       correctTextCounter--;
-      var cText = document.getElementById('correct_text');
-      cText.style.color = 'red';
+    //  incorrectHighlight();
   }
 }
 
-function highlight(){
+function correctHighlight(){
   var instance = new Mark(document.getElementById('correct_text'));
   //highlights correct words
-  instance.markRanges({
-    start : correctTextCounter,
-    length : 1
-  });
+  instance.unmark();
+  instance.markRanges([{
+    start : 0,
+    length : correctTextCounter + 1
+  }]);
+}
 
-  //highlights incorrect
+function incorrectHighlight()
+{
+  var instance = new Mark(document.getElementById('correct_text'));
+  //highlights correct words
+  instance.markRanges([{
+    start : correctTextCounter,
+    length : inputTextCounter
+  }]);
 }
